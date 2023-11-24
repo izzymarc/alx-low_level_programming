9b16c3c3367d50989e083063cb423c6ecc084bdb
ezekielgwamna <izzymarc@gmail.com>
2023-11-23 18:34:59 -0800

    get bits

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,17 +3,17 @@
 /**
  * get_bit - get the value of a bit at a given index
  * @n: number to evaluate
- * @index: index starting from 0, of the bit we want to get
+ * @index: index starts from 0, of the bit we want to get
  * Return: Value of bit at index, or -1 if error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int hold;
+	unsigned long int grip;
 
 	if (index > 64)
 		return (-1);
 
 	hold = n >> index;
 
-	return (hold & 1);
+	return (grip & 1);
 }
