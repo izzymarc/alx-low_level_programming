cad1df936d8c3d753f605adf18e0991a594244b6
ezekielgwamna <izzymarc@gmail.com>
2023-11-23 17:44:23 -0800

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -22,7 +22,7 @@ void print_binary(unsigned long int n)
 		return;
 	}
 
-	for (bit_counter = n, bit_position = 0; 
+	for (bit_counter = n, bit_position = 0;
 		(bit_counter >>= 1) > 0; bit_position++)
 		;
 
